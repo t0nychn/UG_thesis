0612cbe5c101984c36968e77e0bfc190f0fd106b
Tony Chen <zt.chen@hotmail.com>
2022-04-27 22:53:26 +0100

    update pres3

--- run_mecha.py
+++ run_mecha.py
@@ -1,6 +1,7 @@
 from utils.mecha import *
 
-ys = {'DJCI', 'lumber', 'oats', 'palladium', 'rubber', 'gold','hog', 'wheat', 'crude', 'gas', 'GSCI', 'GSNECI', 'GSECI'}
+ys = {'DJCI', 'BCI', 'CRBI', 'CRBI_industrials', 'CRBI_food',	'CRBI_metals', 'CRBI_livestock', 'CRBI_fats', 'CRBI_textiles',
+'lumber', 'oats', 'palladium', 'rubber', 'gold','hog', 'wheat', 'crude', 'gas', 'GSCI', 'GSNECI', 'GSECI'}
 xs = {'SPX', 'MSCI_W', 'MSCI_EM'}
 
 ADAM(ys, xs).run(file_ending='-master')
